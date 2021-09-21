231600e2d42338d51d0f5c9d04e1ea3f19c8a548
Larry Turner <larry.turner@se.com>
2021-09-21 12:53:02 -0400

    Modified requirements.

--- diamondback/__init__.py
+++ diamondback/__init__.py
@@ -74,4 +74,4 @@ from .transforms import PowerSpectrumTransform  # noqa: F401
 from .transforms import WaveletTransform  # noqa: F401
 from .transforms import ZTransform  # noqa: F401
 
-__version__ = '1.0.98'
+__version__ = '1.0.99'
