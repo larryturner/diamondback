18f983ab057e70e5ee6c002a0f1cd306bec10419
Larry Turner <larry.turner@se.com>
2022-08-02 13:54:24 -0400

    Modified setup.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ try :
 except Exception :
     description = ''
 name = 'diamondback'
-with open( name + os.path.sep + '__init__.py', 'r' ) as fin :
+with open( os.path.join( name, '__init__.py' ), 'r' ) as fin :
     label, value = r'__version__\s{0,4}=\s{0,4}', r'\d{1,3}\.\d{1,3}\.\d{1,3}'
     version = re.findall( value, re.findall( label + '\W' + value + '\W', fin.read( ) )[ 0 ] )[ 0 ]
 setup( name = name,
